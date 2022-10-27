edd8957d104c29c5bb4c75c941f6a56086281b5d
todo-actions <todo-actions[bot]@users.noreply.github.com>
2022-10-27 12:59:36 +0000

    chore(todo): collect new todos

--- src/modules/logto.ts
+++ src/modules/logto.ts
@@ -4,7 +4,7 @@ import { createLogto } from '@logto/vue'
 
 export const install: UserModule = ({ app }) => {
   app.use(createLogto, {
-    // TODO: Dodac env vara na produkcje
+    // TODO [$635a80b88b7108067b50a26f]: Dodac env vara na produkcje
     endpoint: `https://3001-${import.meta.env.GITPOD_WORKSPACE_ID}.${import.meta.env.GITPOD_WORKSPACE_CLUSTER_HOST}`,
     appId: 'x7o1RDPyyDlbfokcBzroP',
   })
